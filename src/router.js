6efbfacaf719e32ee90022dff45ba871d31bca1d
Imanishi Tomoya <imanishitomoya@Imanishis-MacBook-Air.local>
2019-05-10 14:14:21 +0900

    add router

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Home from "./views/Home";
 
 Vue.use(Router);
 
@@ -12,15 +12,18 @@ export default new Router({
       path: "/",
       name: "home",
       component: Home
-      // },
-      // {
-      //   path: "/about",
-      //   name: "about",
-      //   // route level code-splitting
-      //   // this generates a separate chunk (about.[hash].js) for this route
-      //   // which is lazy-loaded when the route is visited.
-      //   component: () =>
-      //     import(/* webpackChunkName: "about" */ "./views/About.vue")
+    },
+    {
+      path: "/portfolio",
+      name: "portfolio",
+      component: () =>
+        import(/* webpackChunkName: "portfolio" */ "./views/Portfolio")
+    },
+    {
+      path: "/cv",
+      name: "cv",
+      component: () =>
+        import(/* webpackChunkName: "cv" */ "./views/CV")
     }
   ]
 });
